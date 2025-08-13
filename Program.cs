d6d162299a653e501efea075f76d876bc62911c2
Natluis91 <natluis91@gmail.com>
2025-08-13 17:59:24 +0500

    Resolve merge conflict

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello from feature/greeting!");
+=======
+Console.WriteLine("Hello from MAIN!");
+>>>>>>> f811467 (Add Program.cs in main)
